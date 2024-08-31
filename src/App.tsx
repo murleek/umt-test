63d72494ebb4180fa2cc5a26d5b011b50de194d6
Maksym <r4v3c4t@pm.me>
2024-08-31 13:13:50 +0300

    set up redux store and fetch users

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import "./App.css";
+import { useGetUsersQuery } from "./app/services/user.ts";
 
 function App() {
+  const { data } = useGetUsersQuery();
+
+  console.log(data);
+
   return <span>UMT</span>;
 }
 
