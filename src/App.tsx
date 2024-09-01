0a9439fec2ad4fcbdd25db62e25faece506f8a2b
Maksym <r4v3c4t@pm.me>
2024-09-01 11:22:42 +0300

    created page header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import { useGetUsersQuery } from "./app/services/user.ts";
+import Header from "./components/Header/Header.tsx";
 
 function App() {
   const { data } = useGetUsersQuery();
 
   console.log(data);
 
-  return <span>UMT</span>;
+  return (
+    <>
+      <Header></Header>
+      <main></main>
+    </>
+  );
 }
 
 export default App;
